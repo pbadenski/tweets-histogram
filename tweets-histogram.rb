b26d2d687eea10a9114011897fe8fd9c34d533fb
Pawel Badenski <Pawel.Badenski@gmail.com>
2012-06-06 09:55:24 +0100

    Optimization for number of API calls.
    Maximum of 2000 tweets are considered for histogram now.

--- tweets-histogram.rb
+++ tweets-histogram.rb
@@ -3,15 +3,17 @@ require 'open-uri'
 require 'json'
 require 'progressbar'
 
-def load_tweets(username, pages)
+def load_tweets(username)
+	user = JSON.parse(open("https://api.twitter.com/1/users/show.json?screen_name=#{username}&include_entities=true").lines.reduce)
+	pages = (user["statuses_count"].to_f/200).ceil
 	progressbar = ProgressBar.new("progress", pages)
-	(1 .. pages).map { |i|
+	(1 .. [pages, 10].min).map { |i|
 		progressbar.inc
-		JSON.parse(open("https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=#{username}&count=3200&page=#{i}").lines.reduce)
+		JSON.parse(open("https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=#{username}&count=200&page=#{i}").lines.reduce)
 	}.flatten
 end
 
-tweets = load_tweets(ARGV[0], 32)
+tweets = load_tweets(ARGV[0])
 histogram = tweets.
 	map { |t| DateTime.parse(t["created_at"]).hour }.
 	reduce({}) { |h, hour| h[hour] = (h[hour] or 0) + 1; h }.
